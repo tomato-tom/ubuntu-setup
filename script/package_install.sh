6afce484c68a8c12c40987c1ad95ee0a276e19f1
tomato-tom <hajimeee49@gmail.com>
2024-10-10 11:27:25 +0900

    modified:   package_install.sh

--- script/package_install.sh
+++ script/package_install.sh
@@ -12,5 +12,15 @@ sudo snap install joplin-desktop
 sudo snap install vlc
 sudo snap install yt-dlp
 
+sudo systemctl daemon-reload
+
 # Oh My Bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+# OhMyBashで.bashrc作成されるから、その後Alias追記
+echo "alias py=python3" >> $HOME/.bashrc
+echo "alias python=python3" >> $HOME/.bashrc
+echo "alias clip=\"xclip -selection clipboard\"" >> $HOME/.bashrc
+echo "alias ull=ultralist" >> $HOME/.bashrc
+echo "export PATH=\"$PATH:$HOME.local/bin\"" >> $HOME/.bashrc
+
